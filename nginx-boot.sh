e3a840abb5023a3b607982125522bae888e39241
Honokae <96947283+Honokae@users.noreply.github.com>
2021-09-12 20:15:52 +0000

    Update date.

--- nginx-boot.sh
+++ nginx-boot.sh
@@ -3,4 +3,4 @@
 # Check for variables
 export WORKER_CONNECTIONS=${WORKER_CONNECTIONS:-1024}
 export HTTP_PORT=${HTTP_PORT:-80}
-export REDIRECT=${REDIRECT:-https
+export REDIRECT=${REDIRECT:-https\:\
