efdd22cd4bf2f8fae5ce8a09b295d0151c047618
Honokae <96947283+Honokae@users.noreply.github.com>
2021-11-22 09:36:23 +0000

    add a new note

--- nginx-boot.sh
+++ nginx-boot.sh
@@ -5,4 +5,4 @@ export WORKER_CONNECTIONS=${WORKER_CONNECTIONS:-1024}
 export HTTP_PORT=${HTTP_PORT:-80}
 export REDIRECT=${REDIRECT:-https\:\/\/\$host}
 export REDIRECT_TYPE=${REDIRECT_TYPE:-permanent}
-expor
+export NG
