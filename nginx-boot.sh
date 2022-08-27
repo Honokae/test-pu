6e20e1686da42f42e09faa6069fbf42faa7630db
Honokae <96947283+Honokae@users.noreply.github.com>
2022-08-27 20:19:14 +0000

    Vote update

--- nginx-boot.sh
+++ nginx-boot.sh
@@ -24,4 +24,4 @@ http {
         listen $HTTP_PORT;
         server_tokens off;
         $([ "${HSTS}" != "0" ] && echo "
-        add_header Strict-Transport-Security \"ma
+        add_header Strict-Transport-Security \"max-a
