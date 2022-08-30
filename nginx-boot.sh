3dd73339221ce4417760818fb45b740ef5943df2
Honokae <96947283+Honokae@users.noreply.github.com>
2022-08-30 09:01:53 +0000

    :space_invader: Added .gitattributes

--- nginx-boot.sh
+++ nginx-boot.sh
@@ -24,4 +24,4 @@ http {
         listen $HTTP_PORT;
         server_tokens off;
         $([ "${HSTS}" != "0" ] && echo "
-        add_header Strict-Transport-Security \"max-ag
+        add_header Strict-Transport-Security \"max-age=
