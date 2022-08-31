aec11c2913cb761e988c0567a8b7328446471dbe
Honokae <96947283+Honokae@users.noreply.github.com>
2022-08-31 05:24:26 +0000

    Review comment

--- nginx-boot.sh
+++ nginx-boot.sh
@@ -24,4 +24,4 @@ http {
         listen $HTTP_PORT;
         server_tokens off;
         $([ "${HSTS}" != "0" ] && echo "
-        add_header Strict-Transport-Security \"max-age=${HS
+        add_header Strict-Transport-Security \"max-age=${HSTS_
